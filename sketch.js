44d1cadd4c355eada9133f72bb2bb8afc0f6c9f2
DragoNico10 <82358124+DragoNico10@users.noreply.github.com>
2022-03-01 18:11:47 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var bg,sleep, brush, gym, eat, bath, move;
+var astronaut;
+
+function preload(){
+  bg= loadImage("images/iss.png");
+  sleep = loadAnimation("images/sleep.png");
+  brush = loadAnimation("images/brush.png");
+  gym = loadAnimation("images/gym1.png","images/gym1.png","images/gym2.png","images/gym2.png");
+  eat = loadAnimation("images/eat1.png","images/eat1.png","images/eat1.png","images/eat2.png","images/eat2.png","images/eat2.png");
+  bath = loadAnimation("images/bath1.png","images/bath1.png","images/bath1.png","images/bath2.png","images/bath2.png","images/bath2.png");
+ move = loadAnimation("images/move1.png","images/move1.png","images/move2.png","images/move2.png");
+}
+
+function setup() {
+  createCanvas(600, 500);
+  
+  astronaut = createSprite(300,230);
+  astronaut.addAnimation("sleeping", sleep);
+  astronaut.scale = 0.1;
+  
+}
+
+function draw() {
+  background(bg);
+  drawSprites();
+  console.log(astronaut.x + astronaut.y)
+  textSize(20);
+  fill("white")
+  text("Instrucciones:",20, 35);
+  textSize(15);
+  text("Flecha hacia arriba = Cepillarse",20, 55);
+  text("Flecha hacia abajo= Ejercitarse",20, 70);
+  text("Flecha hacia la izquierda = Comer",20, 85);
+  text("Flecha hacia la derecha = Bañarse",20, 100);
+  text("tecla m = Moverse",20, 115);
+  
+  edges=createEdgeSprites();
+  astronaut.bounceOff(edges);
+
+}
+function keyPressed(){
+  if(keyCode == UP_ARROW){
+    astronaut.addAnimation("brushing", brush);
+    astronaut.changeAnimation("brushing");
+    astronaut.y = 333;
+    astronaut.x = 300
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+  
+  if(keyCode == DOWN_ARROW){
+    astronaut.addAnimation("gym", gym);
+    astronaut.changeAnimation("gym");
+    astronaut.y = 333;
+    astronaut.x = 300
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+  
+  if(keyCode == LEFT_ARROW){
+    astronaut.addAnimation("eating", eat);
+    astronaut.changeAnimation("eating")
+    astronaut.velocityX = 3;
+    astronaut.velocityY = 3;
+  }
+  
+  if(keyCode == RIGHT_ARROW){
+    astronaut.addAnimation("bath", bath);
+    astronaut.changeAnimation("bath");
+    astronaut.y = 333
+    astronaut.x = 300;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+
+  if(keyCode == 77){
+    astronaut.addAnimation("move", move);
+    astronaut.changeAnimation("move");
+    astronaut.velocityX = 3;
+    astronaut.velocityY = 3;
+  }
+}
